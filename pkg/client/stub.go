17d85391daf397ebe3e33ff3c1855186105d50be
shepard <eviltomorrow@163.com>
2022-02-08 17:20:16 +0800

    fix client stub

--- pkg/client/stub.go
+++ pkg/client/stub.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/eviltomorrow/robber-account/internal/server"
@@ -21,10 +20,9 @@ var (
 	EtcdEndpoints = []string{
 		"127.0.0.1:2379",
 	}
-	mut sync.Mutex
 )
 
-func NewClientForAccount() (pb.AccountClient, func(), error) {
+func init() {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   EtcdEndpoints,
 		DialTimeout: 5 * time.Second,
@@ -38,18 +36,17 @@ func NewClientForAccount() (pb.AccountClient, func(), error) {
 		},
 	})
 	if err != nil {
-		return nil, nil, err
+		panic(fmt.Errorf("create etcd client failure, nest error: %v", err))
 	}
 
 	builder := &grpclb.Builder{
 		Client: cli,
 	}
 
-	mut.Lock()
-	defer mut.Unlock()
-
 	resolver.Register(builder)
+}
 
+func NewClientForAccount() (pb.AccountClient, func(), error) {
 	target := fmt.Sprintf("etcd:///%s", server.Key)
 	conn, err := grpc.DialContext(
 		context.Background(),
